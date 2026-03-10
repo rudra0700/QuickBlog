848d4d604a45846860e578cd835898d2926d140d
rudra0700 <baruarudra7@gmail.com>
2026-03-10 15:26:58 +0600

    fix(error) : fixed typo in Comment.js file

--- server/model/Comment.js
+++ server/model/Comment.js
@@ -10,4 +10,4 @@ const commentSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-export const Comment = mongoose.model(Comment", commentSchema);
+export const Comment = mongoose.model("Comment", commentSchema);
